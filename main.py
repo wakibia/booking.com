cc43f691a61e6c27e97a40b8e91416830a43f1a8
wakibia <cyrusmacharia07@gmail.com>
2023-10-11 23:06:24 +0300

    updated files

--- main.py
+++ main.py
@@ -40,60 +40,57 @@ def parse_args():
     return parser.parse_args()
 
 
-# def run_booking_process(driver_path, currency, place, check_in_date, check_out_date, adults, sort_by, output_csv):
-#     bot = None
-try:
-    # Create a list to store all results
-    bot = Booking() #driver_path=driver_path
-    bot.land_first_page()
-    time.sleep(3)
-    bot.close_popup()
-    bot.change_currency("USD") #currency=currency
-    bot.select_place_to_go("Mombasa")
-    bot.select_dates(check_in_date='2023-10-10', check_out_date='2023-10-14') #check_in_date=check_in_date, check_out_date=check_out_date
-    bot.select_adults(3) #adults
-    time.sleep(3)
-    bot.click_search()
-    time.sleep(3)
+def run_booking_process(driver_path, currency, place, check_in_date, check_out_date, adults, sort_by, output_csv):
+    bot = None
+    try:
+        # Create a list to store all results
+        bot = Booking() #driver_path=driver_path
+        bot.land_first_page()
+        time.sleep(3)
+        bot.close_popup()
+        bot.change_currency("USD") #currency=currency
+        bot.select_place_to_go("Mombasa")
+        bot.select_dates(check_in_date='2023-10-10', check_out_date='2023-10-14') #check_in_date=check_in_date, check_out_date=check_out_date
+        bot.select_adults(3) #adults
+        time.sleep(3)
+        bot.click_search()
+        time.sleep(3)
 
-    bot.apply_filtrations('Price (lowest first)') #sort_by
-    time.sleep(3)
+        bot.apply_filtrations('Price (lowest first)') #sort_by
+        time.sleep(3)
 
-    # booking_results = bot.report_results()
-    bot.report_results()
+        booking_results = bot.report_results()
+        bot.report_results()
 
-    #     transposed_results = list(map(list, zip(*booking_results)))
-    #     # Write the results to a CSV file
-    #     with open(output_csv, 'w', newline='') as csvfile:
-    #         csvwriter = csv.writer(csvfile)
-    #
-    #         # Write the header row
-    #         header = ["Hotel Name", "Price", "Rating", "Hotel URL"]
-    #         csvwriter.writerow(header)
-    #
-    #         # Write each result as a row in the CSV file
-    #         for result in transposed_results:
-    #             csvwriter.writerow(result)
-    #
-    # except Exception as e:
-    #     if 'in PATH' in str(e):
-    #         print(str(e))
-    #     else:
-    #         raise
-    # finally:
-    #     if bot is not None:
-    #         bot.quit()
-    #
+        transposed_results = list(map(list, zip(*booking_results)))
+        # Write the results to a CSV file
+        with open(output_csv, 'w', newline='') as csvfile:
+            csvwriter = csv.writer(csvfile)
 
+            # Write the header row
+            header = ["Hotel Name", "Price", "Rating", "Hotel URL"]
+            csvwriter.writerow(header)
 
-# if __name__ == "__main__":
-#     try:
-#         args = parse_args()
-#         run_booking_process(args.driver_path, args.currency, args.place, args.check_in_date, args.check_out_date,
-#                             args.adults, args.sort_by, args.output_csv)
-#     except StaleElementReferenceException:
-#         pass
+            # Write each result as a row in the CSV file
+            for result in transposed_results:
+                csvwriter.writerow(result)
+
+    except Exception as e:
+        if 'in PATH' in str(e):
+            print(str(e))
+        else:
+            raise
+    finally:
+        if bot is not None:
+            bot.quit()
 
 
-except Exception as e:
-    print(f'Exception was: {e}')
+
+if __name__ == "__main__":
+    try:
+        args = parse_args()
+        run_booking_process(args.driver_path, args.currency, args.place, args.check_in_date, args.check_out_date,
+                            args.adults, args.sort_by, args.output_csv)
+
+    except Exception as e:
+        print(f'Exception was: {e}')
